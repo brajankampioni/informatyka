2855a83148eab66272d204740d118be892e5d48c
brajankampioni <73013006+brajankampioni@users.noreply.github.com>
2020-10-25 10:28:26 +0100

    Create zdanie_A.py

---
+++ git remote add origin https:/github.com/brajankampioni/zdanie_A.py
@@ -0,0 +1,15 @@
+!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Przedmiot: Informatyka
+# Kierunek studiów: Inżynieria Transportu
+# Semestr: zimowy
+# Rok akademicki: 2020/2021
+# Data (dzień.miesiąc.rok): 25.10.2020
+# Imię: Brajan
+# Nazwisko: Kampioni
+# Numer albumu ZUT: 50410
+
+"""
+W tym miejscu przepisać temat.
+"""
