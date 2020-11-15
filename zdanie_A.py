7bb007cf71c6b6d6402c5523b64fc2e112ef6e65
Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>
2020-11-15 10:11:58 +0100

    Update zdanie_A.py

--- zdanie_A.py
+++ zdanie_A.py
@@ -1,4 +1,4 @@
-!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Przedmiot: Informatyka
@@ -12,12 +12,18 @@
 
 """
 Suma szeregu
-Napisz program, który dla podanego całkowietego dodatniego N<1000000 oblicza sume szeregu s=a1+a2+...+aN gdzie aK=(1+1/k)/k^3+1)
-
-n=int(input("1000000"))
-sum1=0
-for i in range(1,n+1):
-      sum1=sum1+(1/i)
-print("The sum of series is",round(sum1,2))
-
+Napisz program, który dla podanego całkowietego dodatniego N<1000000 oblicza
+sume szeregu s=a1+a2+...+aN gdzie aK=(1+1/k)/k^3+1)
 """
+
+n = int(input("Podaj ilość wyrazów, nie więcej niż 1000000: "))
+sum1 = 0
+for i in range(1, n + 1):
+      
+      # TODO: zastąpić 1 / i tym co naprawdę ma być liczone
+      # 
+      sum1 = sum1 + (1 / i) 
+
+# TODO: albo anglielski - albo polski - jeżeli chodzi o język naturalny
+#
+print("The sum of series is", round(sum1,2))
